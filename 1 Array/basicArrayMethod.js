fd4755f26285e5bfcf5a4433c3af617264cad3fc
Prashant Gupta <prashantguptaofficial007@gmail.com>
2025-01-08 17:14:44 +0530

    Add basic method of array

---
+++ 1 Array/basicArrayMethod.js
@@ -0,0 +1,25 @@
+let arr = [];
+
+// Add element in array
+
+arr.push("Javascript");
+arr.push("Java");
+arr.push("Swift");
+
+arr.unshift("C++")
+
+// Remove element from array
+
+arr.pop();
+
+arr.shift();
+
+// Index of array
+
+let arr2 = [1,5,3];
+let index = arr2.indexOf(3)
+
+//  Get Element of array using at()
+
+let elm = arr2.at(2)
+console.log(elm);
